9c919273572b6cde9aea7249d33bbe28633cbd23
xavisec <xavisec@protonmail.com>
2025-02-24 23:42:11 +0000

    SignUp Test Version

---
+++ src/Pre/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import SignUpForm from "./komodoSignUpTest.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
