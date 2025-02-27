a07360cf653228209d8775351ccde45aef800845
xavisec <xavisec@protonmail.com>
2025-02-27 10:34:47 +0000

    Structure of directories

---
+++ src/test/components/MainApp/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainLayout from '../MainLayout/MainLayout.jsx';
+import ModulesPage from '../ModulesPage/ModulesPage.jsx';
+
+const App = () => {
+
+
+
+  return (
+    <MainLayout>
+      <ModulesPage />
+    </MainLayout>
+  );
+};
+
+export default App;
